a2429a1527807a98f4bd08071676f1828cb46a9c
Edu Alcalá <ealcala@imac.home>
2013-07-10 19:58:21 +0200

    Added Hello world from index.html file

--- web.js
+++ web.js
@@ -3,10 +3,23 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	writeFile('index.html', response);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/*
+ * Function to output the file 
+ */
+function writeFile(fileName, response) {
+	var fs = require('fs');
+	
+	
+	fs.readFile(fileName, function (err, data) {
+		if (err) throw err;
+		response.send(data.toString("utf-8"));
+	});
+}
